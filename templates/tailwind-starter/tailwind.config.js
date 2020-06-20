216f6ca2259bc9c8af3b50ecb531f4d064a82884
Bodhish Thomas <bodhish@gmail.com>
2020-06-21 02:32:33 +0530

    Update tailwind starter - Bump Reason react version - Add purge for tailwind css

---
+++ templates/tailwind-starter/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {},
+  purge: {
+    content: ["./src/**/*.html", "./src/**/*.re", "./src/**/*.bs.js"],
+  },
+};
+
+// Add the following if you want to whitelist classes
+// options: {
+//   whitelistPatterns: [/^bg-/, /^text-/, /^border-/, /^hover:/],
+// },
