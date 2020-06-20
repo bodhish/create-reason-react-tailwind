8fd98d680c4bb845302312bbce14be88286d00e8
Bodhish Thomas <bodhish@gmail.com>
2020-06-21 02:41:59 +0530

    Update reason react in webpack tailwind starter

--- templates/webpack-tailwind/tailwind.config.js
+++ templates/webpack-tailwind/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: {
+    content: ["./src/**/*.html", "./src/**/*.re", "./src/**/*.bs.js"],
+  },
 };
+
+// Add the following if you want to whitelist classes
+// options: {
+//   whitelistPatterns: [/^bg-/, /^text-/, /^border-/, /^hover:/],
+// },
